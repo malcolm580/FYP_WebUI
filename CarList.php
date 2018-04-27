7d42c3672680c35f8298aa528b9d44cc3b3dc6fd
loc000 <32705197+loc000@users.noreply.github.com>
2018-04-27 17:46:05 +0800

    fuck

--- CarList.php
+++ CarList.php
@@ -1,2 +1,2 @@
-<i class="fa fa-pencil fa-fw w3-margin-right w3-text-theme"></i> E - Sweeper 1<p />
+<i class="fa fa-pencil fa-fw w3-margin-right w3-text-theme"></i> E - Sweeper 1<br />
 <i class="fa fa-pencil fa-fw w3-margin-right w3-text-theme"></i> Rubbish Bin Car 1
